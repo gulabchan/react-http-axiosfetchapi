968be6e9ff83863a5ca452370315e3c1bf6fada2
GULABCHAND KUMAR GUPTA <83635534+gulabchan@users.noreply.github.com>
2023-04-30 16:31:42 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import PostList from "../components/PostList";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <PostList />
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+    </div>
+  );
+}
